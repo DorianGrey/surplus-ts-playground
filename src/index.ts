2ae4d9607ccf10a1b65d6c1bd56bbee119387b1f
Christian Linne <christian.linne@flavia-it.de>
2018-03-01 09:41:08 +0100

    Wb html-webpack-plugin; still attempting to fix HMR ...

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import S from "s-js";
 import { createTestComponent } from "./app/TestComponent";
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 S.root(() => {
   const component = createTestComponent();
 
